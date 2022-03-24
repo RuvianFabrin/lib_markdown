518bb86871ff39a8c55882d95506b0c0f36b15e2
Ruvian <ruvian.fabrin@hotmail.com>
2022-03-24 20:16:56 -0300

    Aula 2 concluida

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const chalk = require('chalk');
+const fs = require('fs');
 
-console.log(chalk.blue("Agora deu certo!!!"));
-
-const paragrafo = 'Texto retornado por uma função';
-
-function texto(string) {
-    return string;
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho informado'));
 }
+//Daqui pra frente só vamos usar async e await
+async function pegaArquivo(caminho) {
+    const encoding = 'utf8';
+    try {
+        const texto = await fs.readFileSync(caminho, encoding);
+        console.log(chalk.green(texto));
+    } catch (erro) {
+        trataErro(erro);
+    }
+    
+}
+/*
+function pegaArquivo(caminho) {
+    const encoding = 'utf8';
+    fs.promises.readFile(caminho, encoding)
+    .then((conteudo) => chalk.green(console.log(conteudo)))
+    .catch((erro) => trataErro(erro));
+}*/
 
-console.log(texto(paragrafo));
+/*
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (erro , texto) => {
+        if (erro) {
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto));
+    })
+}
+*/
+pegaArquivo('./arquivos/texto1.md');
